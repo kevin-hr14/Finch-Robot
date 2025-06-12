21ccf6f66f205f2f47226d7c2d558806e9b242ed
kevin-hr14 <houraokevin@gmail.com>
2025-06-11 23:30:33 -0400

    Update codes.java

--- codes.java
+++ codes.java
@@ -1,1 +1,20 @@
+ Finch f = new Finch();
+        int t = 200;
 
+        while (true) {
+            int lL = f.getLeftLightSensor();
+            int rL = f.getRightLightSensor();
+
+            if (lL < t && rL < t) {
+                f.setWheelVelocities(100, 100);
+            } else if (lL > t && rL < t) {
+                f.setWheelVelocities(50, 100);
+            } else if (lL < t && rL > t) {
+                f.setWheelVelocities(100, 50);
+            } else {
+                f.stopWheels();
+                break;
+            }
+
+            f.sleep(100);
+        }
